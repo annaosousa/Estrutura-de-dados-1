af69101614f537eb712d915c671a6792039fe46b
annaosousa <58178338+annaosousa@users.noreply.github.com>
2020-04-06 14:57:11 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Pilha.h"
+
+int main ()
+{
+    char *palavra = "anna";
+
+    printf(verifica_palindromo(palavra)?"E palindromo":"Nao e palindromo");
+
+}
